39cac77e3a483f26ee600f959bc7e06d59807f5b
mohamed <sam441004@gmail.com>
2026-06-06 02:21:05 +0300

    updated

--- src/modules/users/platform/spaces/dto/create-group.dto.ts
+++ src/modules/users/platform/spaces/dto/create-group.dto.ts
@@ -1,8 +1,31 @@
-import { IsArray, IsMongoId, IsNotEmpty } from 'class-validator';
+import {
+  IsArray,
+  IsMongoId,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class CreateGroupSpaceDto {
   @IsNotEmpty({ message: 'categories.validation.memberId.isNotEmpty' })
   @IsArray({ message: 'categories.validation.members.isArray' })
   @IsMongoId({ each: true, message: 'categories.validation.members.isMongoId' })
   members: string[];
+
+  @IsNotEmpty({ message: 'spaces.validation.name.isNotEmpty' })
+  @IsString({ message: 'spaces.validation.name.isString' })
+  @MinLength(2, { message: 'spaces.validation.name.minLength' })
+  @MaxLength(50, { message: 'spaces.validation.name.maxLength' })
+  name: string;
+
+  @IsOptional()
+  @IsString({ message: 'spaces.validation.description.isString' })
+  @MaxLength(200, { message: 'spaces.validation.description.maxLength' })
+  description?: string;
+
+  @IsOptional()
+  @IsString({ message: 'spaces.validation.avatar.isString' })
+  avatar?: string;
 }
