ff6b04781b8ff0d937e31aed6cc10472061b4fcd
mohamed <sam441004@gmail.com>
2026-07-06 01:43:46 +0300

    updated

--- src/modules/users/platform/comments/comments.module.ts
+++ src/modules/users/platform/comments/comments.module.ts
@@ -3,9 +3,10 @@ import { BaseAuthModule } from '../../../../common/modules/auth/auth.module';
 import { CommentsController } from './comments.controller';
 import { CommentsService } from './comments.service';
 import { BaseCommentsModule } from '../../../../common/modules/platform/comments/comments.module';
+import { BaseMemberModule } from '../../../../common/modules/platform/members/members.module';
 
 @Module({
-  imports: [BaseAuthModule, BaseCommentsModule],
+  imports: [BaseAuthModule, BaseCommentsModule, BaseMemberModule],
   controllers: [CommentsController],
   providers: [CommentsService],
 })
