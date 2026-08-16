ccc4303c2e42af722edde6b6f6c0c6fd85dbda19
mohamed <sam441004@gmail.com>
2026-08-16 04:15:51 +0300

    some new changes

--- src/modules/users/platform/members/members.module.ts
+++ src/modules/users/platform/members/members.module.ts
@@ -4,9 +4,15 @@ import { MembersController } from './members.controller';
 import { BaseAuthModule } from '../../../../common/modules/auth/auth.module';
 import { BaseMemberModule } from '../../../../common/modules/platform/members/members.module';
 import { BaseSpaceModule } from '../../../../common/modules/platform/spaces/spaces.module';
+import { BaseBannedModule } from '../../../../common/modules/platform/banned/banned.module';
 
 @Module({
-  imports: [BaseMemberModule, BaseAuthModule, BaseSpaceModule],
+  imports: [
+    BaseMemberModule,
+    BaseAuthModule,
+    BaseSpaceModule,
+    BaseBannedModule,
+  ],
   controllers: [MembersController],
   providers: [MembersService],
   exports: [MembersService],
